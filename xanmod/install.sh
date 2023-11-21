e5a77ff78bbc7d5d31f0aff8a38d6ff8921a23d6
domaingts <139693268+domaingts@users.noreply.github.com>
2023-11-21 12:39:55 +0000

    update

--- xanmod/install.sh
+++ xanmod/install.sh
@@ -1,7 +1,7 @@
 wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
 
-echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
+echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
 
-sudo apt update && sudo apt install linux-xanmod-x64v3
+apt update && apt install  -y linux-xanmod-x64v3
 
 apt purge gpg
