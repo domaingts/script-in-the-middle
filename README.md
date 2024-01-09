a5dbb36f3ef454c08af8165df5bb3b5079bf75bd
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-09 12:18:52 +0000

    remove

--- README.md
+++
@@ -1,7 +0,0 @@
-# script-in-the-middle
-
-## download docker
-curl -fsSL https://get.docker.com -o get-docker.sh | sh -
-
-## download docker compose file
-curl -fsSL -o docker-compose.yaml https://raw.githubusercontent.com/domaingts/script-in-the-middle/main/docker/docker-test.yaml
--- xanmod/install.sh
+++
@@ -1,9 +0,0 @@
-apt install gpg -y
-
-wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
-
-echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
-
-apt update && apt install  -y linux-xanmod-x64v3
-
-apt purge -y gpg
