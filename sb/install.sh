5cc7f81c8ee56800bfec8ef34cf100a1bef685cf
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-21 08:48:39 +0000

    update

--- sb/install.sh
+++ sb/install.sh
@@ -205,7 +205,7 @@ main() {
     rm_all
   elif [[ "$action" -eq '2' ]]; then
     systemctl stop sing-box && systemctl disable sing-box
-    rm -rf /usr/bin/sing-box /etc/systemd/system/sing-box.service /etc/sing-box
+    rm -rf /etc/sing-box /usr/bin/sing-box /etc/systemd/system/sing-box.service
   elif [[ "$action" -eq '3' ]]; then
     update_sing_box_v3
     rm_all
