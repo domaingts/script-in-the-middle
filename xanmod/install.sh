e676e41e22a786fffc8ae4c9a3813b42cb51c986
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-22 13:10:53 +0000

    update

--- xanmod/install.sh
+++ xanmod/install.sh
@@ -1,9 +1,30 @@
-apt install gpg -y
+#!/bin/bash
 
-wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+version='0'
 
-echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+pre_install() {
+  apt install gnupg2 -y
+}
 
-apt update && apt install -y linux-xanmod-x64v3
+install() {
+  if [[ -z $1 ]]; then
+    echo "$0: unknow parameters"
+    exit 1
+  else
+    wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+    echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+    apt update -y && "apt" "install" "linux-xanmod-x64v$1" "-y"
+  fi
+}
 
-apt purge -y gpg
+post_install() {
+  apt purge -y gpg
+}
+
+main() {
+  pre_install
+  install "$@"
+  post_install
+}
+
+main "$@"
