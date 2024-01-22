178144f6872deed66abcc50114a8a3bcbde556a0
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-22 13:40:33 +0000

    update

---
+++ dd/dd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+password=""
+
+password_generate() {
+  length=$((($RANDOM % 5) + 25))
+  password="$(tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c $length)"
+  "echo" "$password"
+}
+
+dd() {
+  wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/leitbogioro/Tools/master/Linux_reinstall/InstallNET.sh' \
+  && chmod a+x InstallNET.sh \
+  && "bash" "InstallNET.sh" "-debian" "12" "-pwd" "$password"
+}
+
+main() {
+  password_generate
+  dd
+}
+
+main
