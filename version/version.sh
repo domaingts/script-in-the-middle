9dfeadff31a4a3c8223dbfca185e9590a44e9325
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-23 12:15:14 +0000

    update version.sh

--- version/version.sh
+++ version/version.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-kernel_version="$(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'v\K[1-4]{1}')"
+kernel_version=''
 
-echo "$kernel_version"
+get_version() {
+  kernel_version="$(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'v\K[1-4]{1}')"
+}
 
-if [[ $kernel_version == 2 ]]; then
+do_something() {
+  if [[ $kernel_version == 2 ]]; then
     echo "2"
-elif [[ $kernel_version == 4 ]]; then
+  elif [[ $kernel_version == 4 ]]; then
     echo "4"
-else
+  else
     echo "3"
-fi
+  fi
+}
+
+main() {
+  if [[ $1 -gt 0 ]]; then
+    kernel_version=$1
+  else
+    get_version
+  fi
+  do_something
+}
+
+main "$1"
