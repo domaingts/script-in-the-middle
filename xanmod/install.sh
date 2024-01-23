151bec9ec41bf263eb56a63efd3555ec7711dcee
domaingts <139693268+domaingts@users.noreply.github.com>
2024-01-23 12:24:37 +0000

    update xanmod

--- xanmod/install.sh
+++ xanmod/install.sh
@@ -6,15 +6,24 @@ pre_install() {
   apt install gnupg2 -y
 }
 
-install() {
-  if [[ -z $1 ]]; then
-    echo "$0: unknow parameters"
-    exit 1
+get_cpu_version() {
+  version="$(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'v\K[1-4]{1}')"
+}
+
+get_version() {
+   if [[ $1 -gt 0 ]]; then
+    version=$1
   else
-    wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
-    echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
-    apt update -y && "apt" "install" "linux-xanmod-x64v$1" "-y"
+    get_cpu_version
   fi
+}
+
+install() {
+  get_version "$@"
+  echo "$version"
+  wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -o /usr/share/keyrings/xanmod-archive-keyring.gpg
+  echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+  apt update -y && "apt" "install" "linux-xanmod-x64v$version" "-y"
 }
 
 post_install() {
