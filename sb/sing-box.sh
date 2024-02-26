34908a4c9b3ede6331c5343821136f7902b68fd4
domaingts <139693268+domaingts@users.noreply.github.com>
2024-02-26 13:29:46 +0000

    update

--- sb/sing-box.sh
+++ sb/sing-box.sh
@@ -23,6 +23,9 @@ judgement_parameters() {
   'clear')
     action='5'
     ;;
+  'update_pre')
+    action='6'
+    ;;
   *)
     echo "$0: unknow parameters"
     exit 1
@@ -203,6 +206,37 @@ common() {
   sing-box version | tee
 }
 
+pre_update() {
+  TEMPD="$(mktemp -d)"
+  local temp_file
+  temp_file="$(mktemp)"
+  if ! curl -sS -H "Accept: application/vnd.github.v3+json" -o "$temp_file" 'https://api.github.com/repos/SagerNet/sing-box/releases'; then
+    "rm" "$temp_file"
+    echo 'error: Failed to get release list, please check your network.'
+  fi
+  local releases_list
+  releases_list="$(sed 'y/,/\n/' "$temp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
+  "rm" "$temp_file"
+  local i
+  for i in "${!releases_list[@]}"
+  do
+    version="${releases_list[$i]#v}"
+    break
+  done
+  get_cpu_version
+  if [[ $architect == 1 ]]; then
+    package="sing-box-$version-linux-amd64v3"
+  else
+    package="sing-box-$version-linux-amd64"
+  fi
+  curl -L "https://github.com/SagerNet/sing-box/releases/download/v$version/$package.tar.gz" -o "$TEMPD/$package.tar.gz"
+  tar Cxzvf "$TEMPD" "$TEMPD/$package.tar.gz"
+  location="$TEMPD/${package}/sing-box"
+
+  mv "$location" /usr/bin/
+  sing-box version | tee
+}
+
 add_sing_box() {
   add_configuration
   common
@@ -211,6 +245,12 @@ add_sing_box() {
 update_sing_box() {
   systemctl stop sing-box
   common
+  systemctl start sing-box
+}
+
+update_sing_box_to_pre_release() {
+  systemctl stop sing-box
+  pre_update
   systemctl start sing-box
 }
 
@@ -235,6 +275,9 @@ main() {
     journalctl -fu sing-box -o cat
   elif [[ "$action" -eq '5' ]]; then
     journalctl --rotate && journalctl --vacuum-time=1s
+  elif [[ "$action" -eq '6' ]]; then
+    update_sing_box_to_pre_release
+    rm_all
   fi
 }
 
