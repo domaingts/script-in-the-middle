de5e8887361d5bd954fad2ad5c61d6293e2a634e
domaingts <139693268+domaingts@users.noreply.github.com>
2024-10-03 18:57:59 +0800

    Update realm.sh

--- realm/realm.sh
+++ realm/realm.sh
@@ -43,14 +43,14 @@ download() {
     TEMPD="$(mktemp -d)"
     local temp_file
     temp_file="$(mktemp)"
-    if ! curl -sS -H "Accept: application/vnd.github.v3+json" -o "$temp_file" 'https://api.github.com/repos/zhboner/realm/releases/latest'; then
+    if ! curl -sS -H "Accept: application/vnd.github.v3+json" -o "$temp_file" 'https://api.github.com/repos/domaingts/realm/releases/latest'; then
         "rm" "$temp_file"
         echo 'error: Failed to get release list, please check your network.'
     fi
     version="$(sed 'y/,/\n/' "$temp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
     "rm" "$temp_file"
     local package="realm-x86_64-unknown-linux-gnu.tar.gz"
-    curl -L "https://github.com/zhboner/realm/releases/download/$version/$package" -o "$TEMPD/$package"
+    curl -L "https://github.com/domaingts/realm/releases/download/$version/$package" -o "$TEMPD/$package"
     tar Cxzvf "$TEMPD" "$TEMPD/$package"
     location="$TEMPD/realm"
     mv "$location" /usr/bin/
