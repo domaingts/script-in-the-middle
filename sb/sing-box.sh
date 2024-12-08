1460b147bb4a890cbb0219f47fc445c7326f235e
domaingts <139693268+domaingts@users.noreply.github.com>
2024-12-08 01:41:57 +0000

    update

--- sb/sing-box.sh
+++ sb/sing-box.sh
@@ -33,16 +33,16 @@ judgement_parameters() {
   esac
 }
 
-get_cpu_version() {
-  cpu_version="$(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'v\K[1-4]{1}')"
-  if [[ $cpu_version -ge 3 ]]; then
-    architect=1
-  elif [[ -n $(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'amd') ]]; then
-    architect=1
-  else
-    architect=0
-  fi
-}
+# get_cpu_version() {
+#   cpu_version="$(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'v\K[1-4]{1}')"
+#   if [[ $cpu_version -ge 3 ]]; then
+#     architect=1
+#   elif [[ -n $(cat "/proc/cpuinfo" | grep -oP 'model name.*: \K.*' | head -n 1 | grep -oiP 'amd') ]]; then
+#     architect=1
+#   else
+#     architect=0
+#   fi
+# }
 
 add_systemd() {
   cat >/etc/systemd/system/sing-box.service <<EOF
@@ -178,12 +178,13 @@ common() {
   version="$(sed 'y/,/\n/' "$temp_file" | grep 'tag_name' | awk -F '"' '{print $4}')"
   "rm" "$temp_file"
   version="${version#v}"
-  get_cpu_version
-  if [[ $architect == 1 ]]; then
-    package="sing-box-$version-linux-amd64v3"
-  else
-    package="sing-box-$version-linux-amd64"
-  fi
+  package="sing-box-$version-linux-amd64"
+  # get_cpu_version
+  # if [[ $architect == 1 ]]; then
+  #   package="sing-box-$version-linux-amd64v3"
+  # else
+  #   package="sing-box-$version-linux-amd64"
+  # fi
   curl -L "https://github.com/SagerNet/sing-box/releases/download/v$version/$package.tar.gz" -o "$TEMPD/$package.tar.gz"
   tar Cxzvf "$TEMPD" "$TEMPD/$package.tar.gz"
   location="$TEMPD/${package}/sing-box"
