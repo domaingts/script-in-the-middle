e75de4fe998bcf37d27e5c29d243a4475cf979fc
domaingts <139693268+domaingts@users.noreply.github.com>
2026-02-21 03:04:15 +0000

    update

---
+++ xanmod/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt install -y jq
+
+curl "https://api.github.com/repos/domaingts/kernel-build/releases/latest" | jq -r '.assets | .[-2:].[].browser_download_url' | while read -r url; do
+    curl -LO "$url"
+done
+
+dpkg -i linux-image-*xanmod*.deb linux-headers-*xanmod*.deb
+
+rm linux-image-*xanmod*.deb linux-headers-*xanmod*.deb
